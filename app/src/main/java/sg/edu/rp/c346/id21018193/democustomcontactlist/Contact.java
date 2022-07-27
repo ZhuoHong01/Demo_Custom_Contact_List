8dcfcc43af58648ccce36e78c16c57649e71d512
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 10:06:28 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democustomcontactlist/Contact.java
@@ -0,0 +1,59 @@
+package sg.edu.rp.c346.id21018193.democustomcontactlist;
+
+import java.util.Calendar;
+
+public class Contact {
+
+    private String name;
+    private int countryCode;
+    private int phoneNum;
+    private char gender;
+
+    public Contact(String name, int countryCode, int phoneNum, char gender){
+        this.name = name;
+        this.countryCode = countryCode;
+        this.phoneNum = phoneNum;
+        this.gender = gender;
+    }
+
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String toString() {
+        return "Contact {" +
+                "name ='" + name + '\'' +
+                ", gender ='" + gender + '\'' +
+                ", country code ='" + countryCode + '\'' +
+                ", phone number ='" + phoneNum + '\'' +
+                '}';
+    }
+}
